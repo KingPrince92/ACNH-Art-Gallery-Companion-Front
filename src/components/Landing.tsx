bd6dd751871a880957523d2e6192fe52552d854e
Andrew <andrew_nadz12@live.com>
2022-08-29 16:41:10 -0400

    buttons and search

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,30 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Landing.css";
 
 const Landing = () => {
   return (
     <div className="Landing">
       <div className="flexbuttons">
-        <button className="landingbuttons">
-          <Link to="/gallery" className="landingbuttons">
-            Gallery
-          </Link>
-        </button>
-        <button className="landingbuttons">
-          <Link to="/mycollection" className="landingbuttons">
-            My Collection
-          </Link>
-        </button>
-        <button className="landingbuttons">
-          <Link to="/quiz" className="landingbuttons">
-            Quiz
-          </Link>
-        </button>
-        <button className="landingbuttons">
-          <Link to="/aboutus" className="landingbuttons">
-            About Us
-          </Link>
-        </button>
+        <Link to="/gallery" className="landingbuttons">
+          Gallery
+        </Link>
+        <Link to="/mycollection" className="landingbuttons">
+          My Collection
+        </Link>
+        <Link to="/quiz" className="landingbuttons">
+          Quiz
+        </Link>
+        <Link to="/aboutus" className="landingbuttons">
+          About Us
+        </Link>
       </div>
     </div>
   );
