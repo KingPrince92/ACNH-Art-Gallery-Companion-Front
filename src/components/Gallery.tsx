b57d12cc4c3d093d36c250ddf93686b4fa937ac1
Andrew <andrew_nadz12@live.com>
2022-08-29 15:18:24 -0400

    search function

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import SingleArt from "../models/SingleArt";
 import { getArtGallery } from "../services/artService";
 import "./Gallery.css";
+import SearchArtForm from "./SearchArtForm";
 import SingleArtPiece from "./SingleArtPiece";
 
 const Gallery = () => {
   const [art, setArt] = useState<SingleArt[]>([]);
+  const [searchParams] = useSearchParams();
+  const searchTerm = searchParams.get("query") || "";
+  console.log(searchTerm);
   useEffect(() => {
     getArtGallery().then((response) => {
       setArt(response);
@@ -13,10 +18,15 @@ const Gallery = () => {
   }, []);
   return (
     <div className="Gallery">
+      <SearchArtForm />
       <ul>
-        {art.map((item) => (
-          <SingleArtPiece art={item} />
-        ))}
+        {art
+          .filter((item) =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((item) => (
+            <SingleArtPiece art={item} />
+          ))}
       </ul>
     </div>
   );
--- src/components/SearchArtForm.tsx
+++ src/components/SearchArtForm.tsx
@@ -1,11 +1,33 @@
-import './SearchArtForm.css'
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./SearchArtForm.css";
 
 const SearchArtForm = () => {
+  const navigate = useNavigate();
+  const [inquiry, setInquiry] = useState("");
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+    navigate(`/search?${new URLSearchParams({ query: inquiry })}`);
+    setInquiry("");
+  };
+
   return (
-    <div className='SearchArtForm'>
-        SearchArtForm works
-    </div>
-  )
+    <form className="SearchArtForm" onSubmit={(e) => handleSubmit(e)}>
+      <label htmlFor="inquiry"></label>
+      <div className="classinput">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input
+          type="text"
+          name="inquiry"
+          id="inquiry"
+          value={inquiry}
+          onChange={(e) => setInquiry(e.target.value)}
+          placeholder="Search Art"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchArtForm;
