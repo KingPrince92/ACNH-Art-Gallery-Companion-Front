42b386f0d06886306a88be1405a36cfd1210bb9e
Milo <miloprince92@gmail.com>
2022-09-07 09:40:27 -0400

    UserFix

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -1,17 +1,13 @@
-import { User } from "firebase/auth";
 import { createContext } from "react";
 import UserProfile from "../models/userResponse";
+
 export interface AuthContextModel {
-  user: User | null; // null when not logged in
-  profiles: UserProfile[];
-  currentUserProfile: UserProfile | null;
-  updateUserProfiles: () => void;
+  user: UserProfile | null; // null when not logged in
 }
 const defaultValue: AuthContextModel = {
   user: null,
-  profiles: [],
-  currentUserProfile: null,
-  updateUserProfiles: () => {},
 };
 const AuthContext = createContext(defaultValue);
 export default AuthContext;
+
+// updateUserProfiles: () => void;
