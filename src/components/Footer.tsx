f192eb1872cad998f6be6ed9db7e2c9d6a3128fc
Andrew <andrew_nadz12@live.com>
2022-09-07 17:58:46 -0400

    aboutus

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,12 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="Footer"></div>;
+  return (
+    <div className="Footer">
+      <p className="copyright">© 2022 ACNH Companion</p>
+      <p className="nintendo">Property of Nintendo</p>
+    </div>
+  );
 };
 
 export default Footer;
